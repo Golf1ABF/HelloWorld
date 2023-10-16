fd59335e1e5dcba0a52b060274f00c01368edb25
Szó Viktor <C20SzV@szgyatech.local>
2023-10-16 10:54:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Viktor";
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
